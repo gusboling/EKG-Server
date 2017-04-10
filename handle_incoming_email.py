9ee14ab0bb574a091c24397c564dc1a2e79721f2
Augustus Boling <geekish2428@gmail.com>
2017-04-10 15:30:50 -0700

    Emails bodies are now parsed to remove google boiler-plate text

--- handle_incoming_email.py
+++ handle_incoming_email.py
@@ -17,18 +17,20 @@ class LogSenderHandler(InboundMailHandler):
         for content_type, body in plaintext_bodies:
             plaintext = body.decode()
 
+            # Google emails have a bunch of junk data appended to them. This splits the body 
+            #   after the first word of google-text, and only keeps the first part, which contains
+            #   all of our data.
+            parsed_body = str(plaintext.split('YOUR')[0])
+
             ''' DEBUGGING LOGS '''
-            logging.info("Received a message from: " + mail_message.sender)
-            logging.info("Length (Plain Text): %d.", len(plaintext))
-            logging.info("Body (Plain Text): %s", str(plaintext))
+            #logging.info("Received a message from: " + mail_message.sender)
+            #logging.info("Length (Plain Text): %d.", len(plaintext))
+            #logging.info("Body (Plain Text): %s", str(plaintext))
             ''' /DEBUGGING LOGS '''
 
             now = time.strftime("%c")
-            newEntry = models.Packet(source=str(mail_message.sender), body=str(plaintext), date=str(now))
+            newEntry = models.Packet(source=str(mail_message.sender), body=parsed_body, date=str(now))
             newKey = newEntry.put()
-
-
-
 
 
 app = webapp2.WSGIApplication([LogSenderHandler.mapping()], debug=True)
